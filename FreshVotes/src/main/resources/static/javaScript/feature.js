53a3698776ef0dda60e9cbcd01bb9585a0d8def6
Alex Kvasha <alex_kvasha228@gmail.com>
2020-05-20 19:01:11 +0300

    Make comment looks more beautiful

--- FreshVotes/src/main/resources/static/javaScript/feature.js
+++ FreshVotes/src/main/resources/static/javaScript/feature.js
@@ -1,9 +1,12 @@
-textarea = document.querySelector("#textarea"); 
-textarea.addEventListener('input', autoResize, false);
-textarea.style.height = 'auto';
-textarea.style.height = textarea.scrollHeight + 'px'
-  
-function autoResize() { 
-    this.style.height = 'auto'; 
-    this.style.height = this.scrollHeight + 'px'; 
-} 
+textarea1 = document.querySelector("#textarea");
+textarea2 = document.querySelector("#textarea-comment");
+for(textarea : [textarea1, textarea2]){
+	textarea.addEventListener('input', autoResize, false);
+	textarea.style.height = 'auto';
+	textarea.style.height = textarea.scrollHeight + 'px'
+	  
+	function autoResize() { 
+	    this.style.height = 'auto'; 
+	    this.style.height = this.scrollHeight + 'px'; 
+	} 
+}
